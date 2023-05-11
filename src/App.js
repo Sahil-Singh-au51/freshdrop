71c9d39068b675fbaed9fb630eb1c0752a05d168
Admin@DESKTOP-K8BDA6U <sahilarunsingh@gmail.com>
2023-05-11 14:10:37 +0530

    he

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return(
+  <div>
+     <Header />
+     <main className='pt-16'>
+      <Outlet/>
+     </main>
+  </div>
+  );
+}
+
+export default App;
